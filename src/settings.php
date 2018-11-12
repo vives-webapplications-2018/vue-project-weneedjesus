b7fdc5f3336cc279445a2e277970cc5e67137f31
JelleDBZ <jelle.debuyzere@hotmail.be>
2018-11-12 18:40:15 +0100

    Add dependencies and settings for eloquent in slim framework

--- src/settings.php
+++ src/settings.php
@@ -3,7 +3,7 @@ return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
-
+        'determineRouteBeforeAppMiddleware' => false,
         // Renderer settings
         'renderer' => [
             'template_path' => __DIR__ . '/../templates/',
@@ -14,6 +14,17 @@ return [
             'name' => 'slim-app',
             'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
             'level' => \Monolog\Logger::DEBUG,
+        ],
+        // Slim Settings
+        'db' => [
+            'driver' => 'mysql',
+            'host' => 'localhost',
+            'database' => 'database',
+            'username' => 'user',
+            'password' => 'password',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => '',
         ],
     ],
 ];
