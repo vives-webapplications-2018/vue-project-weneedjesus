e885294cc9905a5d13c9423406257d84fc52aa59
JelleDBZ <jelle.debuyzere@hotmail.be>
2018-11-13 09:45:00 +0100

    Add routes for login and register

--- src/routes.php
+++ src/routes.php
@@ -2,13 +2,30 @@
 
 use Slim\Http\Request;
 use Slim\Http\Response;
+use App\Models\Kaffie;
 
-// Routes
-
+// Routes for main page
 $app->get('/', function (Request $request, Response $response, array $args) {
     return $response->withRedirect('/index', $status = null);
 });
 
 $app->get('/index', function (Request $request, Response $response, array $args) {
     return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+//Routes for forms
+$app->post('/register', function (Request $request, Response $response, array $args) {
+    $user = new Kaffie();
+    
+    return $this->renderer->render($response, 'customer.phtml', $args); //TODO: needs to direct to a page where customers can be added and be overviewed.
+});
+
+
+//Routes with overview and other things that could be useful
+$app->get('/login', function (Request $request, Response $response, array $args) {
+    return $this->renderer->render($response, 'login.phtml', $args);
+});
+
+$app->get('/register', function (Request $request, Response $response, array $args) {
+    return $this->renderer->render($response, 'register.phtml', $args);
 });
