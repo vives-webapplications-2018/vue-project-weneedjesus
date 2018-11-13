5863a7e886ec6dbf5f60cb922863fde6a04cfa50
JelleDBZ <jelle.debuyzere@hotmail.be>
2018-11-13 09:06:27 +0100

    Add routes to index.phtml + redirect

--- src/routes.php
+++ src/routes.php
@@ -5,4 +5,10 @@ use Slim\Http\Response;
 
 // Routes
 
+$app->get('/', function (Request $request, Response $response, array $args) {
+    return $response->withRedirect('/main', $status = null);
+});
 
+$app->get('/index', function (Request $request, Response $response, array $args) {
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
