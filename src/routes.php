ee45abea47efa657048d319362b48d283626ce31
JelleDBZ <jelle.debuyzere@hotmail.be>
2018-11-19 16:21:58 +0100

    Made changes to routes.php, functionality and cleaner code

--- src/routes.php
+++ src/routes.php
@@ -3,7 +3,7 @@
 use Slim\Http\Request;
 use Slim\Http\Response;
 use App\Models\Kaffie;
-use App\checkValid;
+use App\Connect\Register;
 
 // Routes for main page
 $app->get('/', function (Request $request, Response $response, array $args) {
@@ -17,25 +17,33 @@ $app->get('/index', function (Request $request, Response $response, array $args)
 //Routes for forms (users)
 $app->post('/register', function (Request $request, Response $response, array $args) {
     $user = new Kaffie();
+    $register = new Register();
+    if(isset($_POST['submit'])){
+        $register->registration($_POST['email'], $_POST['password'],$_POST['confirmpassword'] ,$_POST['name'], $_POST['lastname'], $_POST['address'], 
+        $_POST['zip'], $_POST['city'], $_POST['owner']);
+    }
+   
+
     $array = array(
         //TODO: make an array here with key => value and then make the
         //foreach loop + also check for $args
     );
+/*
     //repetitive code, at a later time it will be implemented as an array
     $user::table('users')->name = $_POST['name'];
     $user::table('users')->lastname = $_POST['lastname'];
     /*We need to check the validation + if already exist in db 
-    We will use other class checkValid*/
-
+    We will use other class register.php*/
+/*
     $user::table('users')->email = $_POST['email'];
     $hash = password_hash($_POST['password'], PASSWORD_DEFAULT).
     $user::table('users')->password = $hash;
-    /**/
+    /**//*
     $user::table('users')->address = $_POST['address'];
     $user::table('users')->zip = $_POST['zip'];
     $user::table('users')->city = $_POST['city'];
     $user::table('users')->owner = $_POST['owner'];
-
+    */
     //TODO: needs to direct to a page where customers can be added and be overviewed.
     return $this->renderer->render($response, 'overview.phtml', $args);
 });
@@ -46,7 +54,6 @@ $app->post('/login', function (Request $request, Response $response, array $args
 
     return $this->renderer->render($response, 'overview.phtml', $args);
 });
-
 
 
 //Routes with overview and other things that could be useful
