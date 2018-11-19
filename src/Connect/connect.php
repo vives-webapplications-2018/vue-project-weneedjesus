f9755254fbc55544aad757e0d27f859bc66b252f
JelleDBZ <jelle.debuyzere@hotmail.be>
2018-11-19 17:29:19 +0100

    Add connect.php to establish connection from mysql to PHP

---
+++ src/Connect/connect.php
@@ -0,0 +1,15 @@
+<?php
+
+//connect.php
+
+//Our MySQL user account.
+define('MYSQL_USER', 'root');
+ 
+//Our MySQL password.
+define('MYSQL_PASSWORD', '');
+ 
+//The server that MySQL is located on.
+define('MYSQL_HOST', 'localhost');
+
+//The name of our database.
+define('MYSQL_DATABASE', 'kaffie');
