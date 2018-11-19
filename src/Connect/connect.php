366763386cf2461af77c7abb11a4eff047cedfe5
JelleDBZ <jelle.debuyzere@hotmail.be>
2018-11-19 17:37:53 +0100

    This script connects to MySQL using the PDO object.

--- src/Connect/connect.php
+++ src/Connect/connect.php
@@ -15,3 +15,17 @@ define('MYSQL_HOST', 'localhost');
 
 //The name of our database.
 define('MYSQL_DATABASE', 'kaffie');
+
+/* PDO options / configuration details */
+$pdoOptions = array(
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_EMULATE_PREPARES => false
+);
+
+/* Connect to MySQL and instantiate the PDO object. */ 
+$pdo = new PDO(
+    "mysql:host=" . MYSQL_HOST . ";dbname=" . MYSQL_DATABASE, //DSN
+    MYSQL_USER, //Username
+    MYSQL_PASSWORD, //Password
+    $pdoOptions //Options
+);
