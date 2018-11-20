304ce3bef6d39e0b5625a07dd308ad8b5871fb77
JelleDBZ <jelle.debuyzere@hotmail.be>
2018-11-20 11:01:23 +0100

    Changing how to add in database

--- src/Models/User.php
+++ src/Models/User.php
@@ -7,4 +7,27 @@ use Illuminate\Database\Eloquent\Model;
 class User extends Model 
 {
     public $timestamps = false;
+
+    function trim($array){
+        foreach($array as $item){
+                $item = !empty($item) ? trim($item) : null;
+                $newArr = array();
+                array_push($newArr, $item);
+        }
+        return $newArr;
+    }
+
+    public function valid($em){
+        if (!filter_var($em, FILTER_VALIDATE_EMAIL)){ //gives invalid email format when valid
+            echo "Invalid email format";
+        }
+        return !filter_var($em, FILTER_VALIDATE_EMAIL) === false;
+    }
+
+    public function validPw($pw, $cpw){
+        if($pw == $cpw){
+            return md5($pw);
+        }
+        return "Passwords don't match!";
+    }
 }
--- src/routes.php
+++ src/routes.php
@@ -18,7 +18,10 @@ $app->get('/index', function (Request $request, Response $response, array $args)
 //Routes for forms (users)
 $app->post('/register', function (Request $request, Response $response, array $args) {
     $user = new User();
-    $user->email = $request->getParam('email');
+
+    if($user->valid($request->getParam('email'))) {
+        $user->email = $request->getParam('email');
+    }
     $user->password = $request->getParam('password');
     $user->lastname = $request->getParam('last_name');
     $user->firstname = $request->getParam('first_name');
@@ -27,27 +30,6 @@ $app->post('/register', function (Request $request, Response $response, array $a
     $user->city = $request->getParam('city');
 
     $user->save();
-
-    
-    
-    
-    //$user = new Kaffie();
-    //$register = new Register($_POST['email'], $_POST['password'],$_POST['confirmpassword'] ,$_POST['first_name'], $_POST['last_name'], $_POST['address'], 
-//$_POST['zip'], $_POST['city']/*, $_POST['owner']*/);
-    echo "Hello test"; //test failing
-/*
-    $user::table('users')->name = $_POST['name'];
-    $user::table('users')->lastname = $_POST['lastname'];
-/*
-    $user::table('users')->email = $_POST['email'];
-    $hash = password_hash($_POST['password'], PASSWORD_DEFAULT).
-    $user::table('users')->password = $hash;
-    $user::table('users')->address = $_POST['address'];
-    $user::table('users')->zip = $_POST['zip'];
-    $user::table('users')->city = $_POST['city'];
-    $user::table('users')->owner = $_POST['owner'];
-    */
-
     return $this->renderer->render($response, 'overview.phtml', $args);
 });
 
@@ -61,7 +43,7 @@ $app->post('/login', function (Request $request, Response $response, array $args
 
 //Routes with overview and other things that could be useful
 $app->get('/login', function (Request $request, Response $response, array $args) {
-    return $this->renderer->render($response, 'login.phtml', $args);
+    return $this->renderer->render($response, 'overview.phtml', $args);
 });
 
 $app->get('/register', function (Request $request, Response $response, array $args) {
