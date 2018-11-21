a7499174c016f7ddcdd557240bb9765323e70ecd
JelleDBZ <jelle.debuyzere@hotmail.be>
2018-11-21 08:45:41 +0100

    Add function to check if user already exists

--- src/Models/User.php
+++ src/Models/User.php
@@ -4,25 +4,41 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model 
+class User extends Model
 {
     public $timestamps = false;
 
-
-    function trim($item){
+    public function trim($item)
+    {
         $trimmed = !empty($item) ? trim($item) : null;
         return $trimmed;
     }
 //TODO: check if email already exists in db
-    public function valid($em){
-        if (!filter_var($em, FILTER_VALIDATE_EMAIL)){ 
+    public function valid($em)
+    {
+        if (!filter_var($em, FILTER_VALIDATE_EMAIL)) {
             echo "Invalid email format";
+            return false;
+        }else  {
+            if($this->emExists($em)){
+                return !filter_var($em, FILTER_VALIDATE_EMAIL) === false;
+            }
         }
-        return !filter_var($em, FILTER_VALIDATE_EMAIL) === false;
     }
 
-    public function validPw($pw, $cpw){
-        if($pw == $cpw){
+    public function emExists($em)
+    {
+        if (User::where('email', '=', $em)->exists()) {
+            echo "E-mail already exists";
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public function validPw($pw, $cpw)
+    {
+        if ($pw == $cpw) {
             return md5($pw);
         }
         return "Passwords don't match!";
