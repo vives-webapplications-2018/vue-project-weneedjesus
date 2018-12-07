49122a42d18b4aeda578b0fe659e82555463800d
JelleDBZ <jelle.debuyzere@hotmail.be>
2018-12-07 13:55:22 +0100

    Added middleware so userobject would be accessible on all pages

--- src/middleware.php
+++ src/middleware.php
@@ -1,4 +1,20 @@
 <?php
+
+use App\Models\User;
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+$app->add(function ($request, $response, $next) {
+
+
+    if($this->session->get('user_id')) {
+        $this->user = User::find($this->session->get('user_id'));
+        $request = $request->withAttribute('user', $this->user);
+        // $view = $this->get('view');
+        // $view->offsetSet('user', $this->user);
+       
+    }
+    $response = $next($request, $response);
+    return $response;
+});
