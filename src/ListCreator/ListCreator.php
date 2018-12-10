639e8e75670ddcacee92d1a7273d10e7a049724d
Jens Vercruysse <jensvercruysse@outlook.com>
2018-12-11 00:15:05 +0100

    made a custom licreator that can create a nice looking list

--- src/ListCreator/ListCreator.php
+++ src/ListCreator/ListCreator.php
@@ -1,3 +1,57 @@
 <?php
 
+namespace App\ListCreator;
+
+use \JensVercruysse\HtmlElements\H4;
+use \JensVercruysse\HtmlElements\Ul;
+use \JensVercruysse\HtmlElements\Li;
+use \JensVercruysse\HtmlElements\Div;
+use \JensVercruysse\HtmlElements\I;
+use \JensVercruysse\HtmlElements\A;
+
+class ListCreator{
+
+    public function __construct($fileRoute = "", $id = [], $title = "", $input = [])
+    {
+        $this->fileRoute = $fileRoute;
+        $this->id = $id;
+        $this->title = $title;
+        $this->input = $input;
+    }
+
+    public function __toString()
+    {
+        $listTitleContent = new H4($this->title, ["class" => "center-align"]);
+        $listTitle = new Li($listTitleContent, ["class" => "collection-header"]);
+        
+        $output = $this->createList();
+
+        $ul = new Ul($listTitle . $output, ["class" => "collection with-header"]);
+        $this->content = new Div($ul, ["class" => "container"]);
+
+        return (string) $this->content;
+    }
+
+    public function createList()
+    {
+        $output = "";
+        $counter = 0;
+
+        foreach($this->input as $listItem)
+        {
+            $currentId = $this->id[$counter];
+
+            $listIcon = new I("edit", ["class" => "material-icons"]);
+            $listLink = new A($listIcon, ["href" => $this->fileRoute . $currentId, "class" => "secondary-content"]);
+            $listIconDiv = new Div($listItem . $listLink);
+            $li = new Li($listIconDiv, ["class" => "collection-item"]);
+
+            $output.= $li;
+            $counter++;
+        }
+
+        return $output;
+
+    }
+}
 
