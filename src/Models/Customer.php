b03746f3857f760de58b046b162ff0ffa35e4257
JelleDBZ <jelle.debuyzere@hotmail.be>
2018-12-11 09:25:13 +0100

    Trim method + deleted template

--- src/Models/Customer.php
+++ src/Models/Customer.php
@@ -4,7 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Customer extends Model 
+class Customer extends Model
 {
     public $timestamps = false;
+
+    public function trim($item)
+    {
+        $replaced = $item_replaceAll("\\s+", "", " ");
+        $trimmed = !empty($replaced) ? trim($replaced) : null;
+        return $trimmed;
+    }
+
 }
